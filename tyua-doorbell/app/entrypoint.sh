7c4963a15575cb04534a270c9b329f99c9d147ff
paulloft <p.veselovsky@dental-pro.online>
2023-12-29 10:52:02 +0400

    update docker conf

---
+++ tyua-doorbell/app/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/with-contenv bashio
+
+TUYA_CLIENT_ID=$(bashio::config 'tuya_client_id')
+TUYA_CLIENT_SECRET=$(bashio::config 'tuya_client_secret')
+TUYA_DEVICE_ID=$(bashio::config 'tuya_device_id')
+TUYA_REGION=$(bashio::config 'tuya_region')
+WEBHOOK_URL=$(bashio::config 'webhook_url')
+
+node index.js
